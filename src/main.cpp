2611e65c6ad82ecc9c4d260445a3648812283f20
vicfred <git@vicfred.dev>
2025-08-29 12:53:19 -0600

    use spdlog instead of iostream

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@
 #include <tgbot/types/InputFile.h>
 #include <tgbot/types/Message.h>
 
+#include "spdlog/spdlog.h"
+
 using namespace std;
 using namespace TgBot;
 
@@ -49,8 +51,11 @@ static string shell_quote(const string &s) {
 }
 
 int main() {
+  spdlog::set_level(spdlog::level::info);
+  spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
+
   string token(getenv("TOKEN"));
-  cout << "Token: " << token << endl;
+  spdlog::info("Token: {}", token);
   Bot bot(token);
 
   struct Job { int64_t chatId; string url; string user; };
@@ -61,7 +66,7 @@ int main() {
 
   auto process = [&](const Job &job) {
     string url = job.url;
-    cout << "got: " << url << " from id: " << job.user << endl;
+    spdlog::info("got: {} from id: {}", url, job.user);
 
     string qurl = shell_quote(url);
     string ofmt = "%(title)s.%(ext)s";
@@ -73,10 +78,10 @@ int main() {
     string getname_cmd = "youtube-dl --get-filename " + flags + qurl;
     string download_cmd = "youtube-dl " + flags + qurl;
 
-    cout << "Getting the filename" << endl;
+    spdlog::info("Getting the filename");
     FILE *pipe = popen(getname_cmd.c_str(), "r");
     if (!pipe) {
-      cerr << "filename popen failed" << endl;
+      spdlog::error("filename popen failed");
       return;
     }
 
@@ -84,30 +89,30 @@ int main() {
     string filename;
     if (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
       filename = trim_newline(buffer);
-      cout << "filename: " << filename << endl;
+      spdlog::info("filename: {}", filename);
     }
     int ret = pclose(pipe);
-    cout << "Exit code: " << ret << endl;
+    spdlog::debug("get-filename exit code: {}", ret);
     if (filename.empty()) {
-      cerr << "empty filename" << endl;
+      spdlog::error("empty filename");
       return;
     }
 
-    cout << "Downloading the video" << endl;
+    spdlog::info("Downloading the video");
     pipe = popen(download_cmd.c_str(), "r");
     if (!pipe) {
-      cerr << "download popen failed" << endl;
+      spdlog::error("download popen failed");
       return;
     }
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-      cout << buffer;
+      spdlog::debug("dl: {}", string(buffer));
     }
     ret = pclose(pipe);
-    cout << "Exit code: " << ret << endl;
+    spdlog::debug("download exit code: {}", ret);
 
     ifstream test(filename);
     if (!test) {
-      cerr << "file not found: " << filename << endl;
+      spdlog::error("file not found: {}", filename);
       return;
     }
 
@@ -115,14 +120,14 @@ int main() {
     try {
       filesystem::create_directories(outDir);
     } catch (const exception &e) {
-      cerr << "cannot create output dir: " << e.what() << endl;
+      spdlog::error("cannot create output dir: {}", e.what());
       return;
     }
 
     string basename = filesystem::path(filename).filename();
     string outPath = outDir + "/" + basename;
 
-    cout << "Re-encoding to " << outPath << " with x264 CRF 21, preset slow; libfdk_aac 128k" << endl;
+    spdlog::info("Re-encoding to {} with x264 CRF 21, preset slow; libfdk_aac 128k", outPath);
 
     string qin = shell_quote(filename);
     string qout = shell_quote(outPath);
@@ -132,40 +137,40 @@ int main() {
                      qout + " 2>&1";
     pipe = popen(enc_cmd.c_str(), "r");
     if (!pipe) {
-      cerr << "encode popen failed" << endl;
+      spdlog::error("encode popen failed");
       return;
     }
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-      cout << buffer;
+      spdlog::debug("ffmpeg: {}", string(buffer));
     }
     ret = pclose(pipe);
-    cout << "ffmpeg exit code: " << ret << endl;
+    spdlog::debug("ffmpeg exit code: {}", ret);
     if (ret != 0) {
-      cerr << "encoding failed" << endl;
+      spdlog::error("encoding failed");
       return;
     }
 
     try {
       filesystem::remove(filename);
     } catch (const exception &e) {
-      cerr << "warning: could not remove original file " << filename << ": " << e.what() << endl;
+      spdlog::error("could not remove original file {}: {}", filename, e.what());
     }
 
-    cout << "Sending video" << endl;
+    spdlog::info("Sending video");
     bot.getApi().sendVideo(job.chatId, InputFile::fromFile(outPath, "video/mp4"));
 
-    cout << "Video info:" << endl;
+    spdlog::debug("Video info:");
     string video_info_cmd = "ffmpeg -hide_banner -i " + shell_quote(outPath) + " 2>&1";
     pipe = popen(video_info_cmd.c_str(), "r");
     if (!pipe) {
-      cerr << "ffmpeg info popen failed" << endl;
+      spdlog::error("ffmpeg info popen failed");
       return;
     }
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-      cout << buffer;
+      spdlog::debug("info: {}", string(buffer));
     }
     ret = pclose(pipe);
-    cout << "Exit code: " << ret << endl;
+    spdlog::debug("info exit code: {}", ret);
   };
 
   unsigned n = thread::hardware_concurrency();
@@ -197,19 +202,20 @@ int main() {
       q.push(job);
     }
     cv.notify_one();
+    spdlog::info("queued job for chat {}", job.chatId);
   });
 
   signal(SIGINT, on_sigint);
 
   try {
-    cout << "Bot username: " << bot.getApi().getMe()->username << endl;
+    spdlog::info("Bot username: {}", bot.getApi().getMe()->username);
     bot.getApi().deleteWebhook();
     TgLongPoll longPoll(bot);
     while (true) {
       longPoll.start();
     }
   } catch (exception &e) {
-    cout << "error: " << e.what() << endl;
+    spdlog::error("error: {}", e.what());
   }
 
   {
